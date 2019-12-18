samzeter/strongswan

proposal: Generalize KE methods

src/libcharon/sa/ikev1/tasks/quick_mode.c
@@ -796,13 +796,13 @@ static linked_list_t *get_proposals(private_quick_mode_t *this,
 	{
 		if (group != MODP_NONE)
 		{
-			if (!proposal->has_ke_method(proposal, group))
+			if (!proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP, group))
 			{
 				list->remove_at(list, enumerator);
 				proposal->destroy(proposal);
 				continue;
 			}
-			proposal->promote_ke_method(proposal, group);
+			proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP, group);
 		}
 		proposal->set_spi(proposal, this->spi_i);
 	}

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -316,7 +316,8 @@ static bool update_and_check_proposals(private_child_create_t *this)
 		if (this->dh_group != MODP_NONE)
 		{	/* proposals that don't contain the selected group are
 			 * moved to the back */
-			if (!proposal->promote_ke_method(proposal, this->dh_group))
+			if (!proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP,
+											 this->dh_group))
 			{
 				this->proposals->remove_at(this->proposals, enumerator);
 				other_dh_groups->insert_last(other_dh_groups, proposal);
@@ -596,7 +597,8 @@ static status_t select_and_install(private_child_create_t *this,
 	}
 	this->child_sa->set_proposal(this->child_sa, this->proposal);
 
-	if (!this->proposal->has_ke_method(this->proposal, this->dh_group))
+	if (!this->proposal->has_transform(this->proposal, DIFFIE_HELLMAN_GROUP,
+									   this->dh_group))
 	{
 		uint16_t group;
 

src/libcharon/sa/ikev2/tasks/ike_init.c
@@ -332,7 +332,8 @@ static bool build_payloads(private_ike_init_t *this, message_t *message)
 				proposal->set_spi(proposal, id->get_initiator_spi(id));
 			}
 			/* move the selected DH group to the front of the proposal */
-			if (!proposal->promote_ke_method(proposal, this->dh_group))
+			if (!proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP,
+											 this->dh_group))
 			{	/* the proposal does not include the group, move to the back */
 				proposal_list->remove_at(proposal_list, enumerator);
 				other_dh_groups->insert_last(other_dh_groups, proposal);
@@ -614,7 +615,8 @@ static void process_payloads(private_ike_init_t *this, message_t *message)
 	}
 
 	if (ke_payload && this->proposal &&
-		this->proposal->has_ke_method(this->proposal, this->dh_group))
+		this->proposal->has_transform(this->proposal, DIFFIE_HELLMAN_GROUP,
+									  this->dh_group))
 	{
 		if (!this->initiator)
 		{
@@ -826,7 +828,8 @@ METHOD(task_t, build_r, status_t,
 	}
 
 	if (this->dh == NULL ||
-		!this->proposal->has_ke_method(this->proposal, this->dh_group))
+		!this->proposal->has_transform(this->proposal, DIFFIE_HELLMAN_GROUP,
+									   this->dh_group))
 	{
 		uint16_t group;
 
@@ -1061,7 +1064,8 @@ METHOD(task_t, process_i, status_t,
 	}
 
 	if (this->dh == NULL ||
-		!this->proposal->has_ke_method(this->proposal, this->dh_group))
+		!this->proposal->has_transform(this->proposal, DIFFIE_HELLMAN_GROUP,
+									   this->dh_group))
 	{
 		DBG1(DBG_IKE, "peer DH group selection invalid");
 		return FAILED;

src/libstrongswan/tests/suites/test_proposal.c
@@ -274,38 +274,75 @@ START_TEST(test_select_proposal)
 }
 END_TEST
 
-START_TEST(test_promote_ke_method)
+START_TEST(test_has_transform)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
-	ck_assert(proposal->promote_ke_method(proposal, ECP_256_BIT));
+	ck_assert(proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP,
+									  MODP_3072_BIT));
+	ck_assert(proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP,
+									  ECP_256_BIT));
+	ck_assert(!proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP,
+									   MODP_2048_BIT));
+	proposal->destroy(proposal);
+}
+END_TEST
+
+START_TEST(test_has_transform_none)
+{
+	proposal_t *proposal;
+
+	proposal = proposal_create_from_string(PROTO_ESP,
+										   "aes128-sha256");
+	ck_assert(proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP,
+									  MODP_NONE));
+	proposal->destroy(proposal);
+
+	proposal = proposal_create_from_string(PROTO_ESP,
+										   "aes128-sha256-modp3072");
+	ck_assert(!proposal->has_transform(proposal, DIFFIE_HELLMAN_GROUP,
+									   MODP_NONE));
+	proposal->destroy(proposal);
+}
+END_TEST
+
+START_TEST(test_promote_transform)
+{
+	proposal_t *proposal;
+
+	proposal = proposal_create_from_string(PROTO_IKE,
+										   "aes128-sha256-modp3072-ecp256");
+	ck_assert(proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP,
+										  ECP_256_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/ECP_256/MODP_3072");
 	proposal->destroy(proposal);
 }
 END_TEST
 
-START_TEST(test_promote_ke_method_already_front)
+START_TEST(test_promote_transform_already_front)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
-	ck_assert(proposal->promote_ke_method(proposal, MODP_3072_BIT));
+	ck_assert(proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP,
+										  MODP_3072_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/MODP_3072/ECP_256");
 	proposal->destroy(proposal);
 }
 END_TEST
 
-START_TEST(test_promote_ke_method_not_contained)
+START_TEST(test_promote_transform_not_contained)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
 
-	ck_assert(!proposal->promote_ke_method(proposal, MODP_2048_BIT));
+	ck_assert(!proposal->promote_transform(proposal, DIFFIE_HELLMAN_GROUP,
+										   MODP_2048_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/MODP_3072/ECP_256");
 	proposal->destroy(proposal);
 }
@@ -474,10 +511,15 @@ Suite *proposal_suite_create()
 						countof(select_proposal_data));
 	suite_add_tcase(s, tc);
 
-	tc = tcase_create("promote_ke_method");
-	tcase_add_test(tc, test_promote_ke_method);
-	tcase_add_test(tc, test_promote_ke_method_already_front);
-	tcase_add_test(tc, test_promote_ke_method_not_contained);
+	tc = tcase_create("has_transform");
+	tcase_add_test(tc, test_has_transform);
+	tcase_add_test(tc, test_has_transform_none);
+	suite_add_tcase(s, tc);
+
+	tc = tcase_create("promote_transform");
+	tcase_add_test(tc, test_promote_transform);
+	tcase_add_test(tc, test_promote_transform_already_front);
+	tcase_add_test(tc, test_promote_transform_not_contained);
 	suite_add_tcase(s, tc);
 
 	tc = tcase_create("unknown transform types");

bingfengfeifei/strongswan-vpp

src/libcharon/sa/ikev1/tasks/quick_mode.c
@@ -796,13 +796,13 @@ static linked_list_t *get_proposals(private_quick_mode_t *this,
 	{
 		if (group != MODP_NONE)
 		{
-			if (!proposal->has_ke_method(proposal, group))
+			if (!proposal->has_transform(proposal, KEY_EXCHANGE_METHOD, group))
 			{
 				list->remove_at(list, enumerator);
 				proposal->destroy(proposal);
 				continue;
 			}
-			proposal->promote_ke_method(proposal, group);
+			proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD, group);
 		}
 		proposal->set_spi(proposal, this->spi_i);
 	}

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -316,7 +316,8 @@ static bool update_and_check_proposals(private_child_create_t *this)
 		if (this->dh_group != MODP_NONE)
 		{	/* proposals that don't contain the selected group are
 			 * moved to the back */
-			if (!proposal->promote_ke_method(proposal, this->dh_group))
+			if (!proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD,
+											 this->dh_group))
 			{
 				this->proposals->remove_at(this->proposals, enumerator);
 				other_dh_groups->insert_last(other_dh_groups, proposal);
@@ -596,7 +597,8 @@ static status_t select_and_install(private_child_create_t *this,
 	}
 	this->child_sa->set_proposal(this->child_sa, this->proposal);
 
-	if (!this->proposal->has_ke_method(this->proposal, this->dh_group))
+	if (!this->proposal->has_transform(this->proposal, KEY_EXCHANGE_METHOD,
+									   this->dh_group))
 	{
 		uint16_t group;
 

src/libcharon/sa/ikev2/tasks/ike_init.c
@@ -332,7 +332,8 @@ static bool build_payloads(private_ike_init_t *this, message_t *message)
 				proposal->set_spi(proposal, id->get_initiator_spi(id));
 			}
 			/* move the selected DH group to the front of the proposal */
-			if (!proposal->promote_ke_method(proposal, this->dh_group))
+			if (!proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD,
+											 this->dh_group))
 			{	/* the proposal does not include the group, move to the back */
 				proposal_list->remove_at(proposal_list, enumerator);
 				other_dh_groups->insert_last(other_dh_groups, proposal);
@@ -614,7 +615,8 @@ static void process_payloads(private_ike_init_t *this, message_t *message)
 	}
 
 	if (ke_payload && this->proposal &&
-		this->proposal->has_ke_method(this->proposal, this->dh_group))
+		this->proposal->has_transform(this->proposal, KEY_EXCHANGE_METHOD,
+									  this->dh_group))
 	{
 		if (!this->initiator)
 		{
@@ -826,7 +828,8 @@ METHOD(task_t, build_r, status_t,
 	}
 
 	if (this->dh == NULL ||
-		!this->proposal->has_ke_method(this->proposal, this->dh_group))
+		!this->proposal->has_transform(this->proposal, KEY_EXCHANGE_METHOD,
+									   this->dh_group))
 	{
 		uint16_t group;
 
@@ -1061,7 +1064,8 @@ METHOD(task_t, process_i, status_t,
 	}
 
 	if (this->dh == NULL ||
-		!this->proposal->has_ke_method(this->proposal, this->dh_group))
+		!this->proposal->has_transform(this->proposal, KEY_EXCHANGE_METHOD,
+									   this->dh_group))
 	{
 		DBG1(DBG_IKE, "peer DH group selection invalid");
 		return FAILED;

src/libstrongswan/tests/suites/test_proposal.c
@@ -274,38 +274,75 @@ START_TEST(test_select_proposal)
 }
 END_TEST
 
-START_TEST(test_promote_ke_method)
+START_TEST(test_has_transform)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
-	ck_assert(proposal->promote_ke_method(proposal, ECP_256_BIT));
+	ck_assert(proposal->has_transform(proposal, KEY_EXCHANGE_METHOD,
+									  MODP_3072_BIT));
+	ck_assert(proposal->has_transform(proposal, KEY_EXCHANGE_METHOD,
+									  ECP_256_BIT));
+	ck_assert(!proposal->has_transform(proposal, KEY_EXCHANGE_METHOD,
+									   MODP_2048_BIT));
+	proposal->destroy(proposal);
+}
+END_TEST
+
+START_TEST(test_has_transform_none)
+{
+	proposal_t *proposal;
+
+	proposal = proposal_create_from_string(PROTO_ESP,
+										   "aes128-sha256");
+	ck_assert(proposal->has_transform(proposal, KEY_EXCHANGE_METHOD,
+									  MODP_NONE));
+	proposal->destroy(proposal);
+
+	proposal = proposal_create_from_string(PROTO_ESP,
+										   "aes128-sha256-modp3072");
+	ck_assert(!proposal->has_transform(proposal, KEY_EXCHANGE_METHOD,
+									   MODP_NONE));
+	proposal->destroy(proposal);
+}
+END_TEST
+
+START_TEST(test_promote_transform)
+{
+	proposal_t *proposal;
+
+	proposal = proposal_create_from_string(PROTO_IKE,
+										   "aes128-sha256-modp3072-ecp256");
+	ck_assert(proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD,
+										  ECP_256_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/ECP_256/MODP_3072");
 	proposal->destroy(proposal);
 }
 END_TEST
 
-START_TEST(test_promote_ke_method_already_front)
+START_TEST(test_promote_transform_already_front)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
-	ck_assert(proposal->promote_ke_method(proposal, MODP_3072_BIT));
+	ck_assert(proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD,
+										  MODP_3072_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/MODP_3072/ECP_256");
 	proposal->destroy(proposal);
 }
 END_TEST
 
-START_TEST(test_promote_ke_method_not_contained)
+START_TEST(test_promote_transform_not_contained)
 {
 	proposal_t *proposal;
 
 	proposal = proposal_create_from_string(PROTO_IKE,
 										   "aes128-sha256-modp3072-ecp256");
 
-	ck_assert(!proposal->promote_ke_method(proposal, MODP_2048_BIT));
+	ck_assert(!proposal->promote_transform(proposal, KEY_EXCHANGE_METHOD,
+										   MODP_2048_BIT));
 	assert_proposal_eq(proposal, "IKE:AES_CBC_128/HMAC_SHA2_256_128/PRF_HMAC_SHA2_256/MODP_3072/ECP_256");
 	proposal->destroy(proposal);
 }
@@ -474,10 +511,15 @@ Suite *proposal_suite_create()
 						countof(select_proposal_data));
 	suite_add_tcase(s, tc);
 
-	tc = tcase_create("promote_ke_method");
-	tcase_add_test(tc, test_promote_ke_method);
-	tcase_add_test(tc, test_promote_ke_method_already_front);
-	tcase_add_test(tc, test_promote_ke_method_not_contained);
+	tc = tcase_create("has_transform");
+	tcase_add_test(tc, test_has_transform);
+	tcase_add_test(tc, test_has_transform_none);
+	suite_add_tcase(s, tc);
+
+	tc = tcase_create("promote_transform");
+	tcase_add_test(tc, test_promote_transform);
+	tcase_add_test(tc, test_promote_transform_already_front);
+	tcase_add_test(tc, test_promote_transform_not_contained);
 	suite_add_tcase(s, tc);
 
 	tc = tcase_create("unknown transform types");