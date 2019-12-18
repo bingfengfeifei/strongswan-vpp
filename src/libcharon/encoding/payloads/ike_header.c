kenyon/strongswan

wip: ike-header: Add IKE_INTERMEDIATE exchange type

src/libcharon/encoding/payloads/ike_header.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 Tobias Brunner
+ * Copyright (C) 2007-2019 Tobias Brunner
  * Copyright (C) 2005-2006 Martin Willi
  * Copyright (C) 2005 Jan Hutter
  * HSR Hochschule fuer Technik Rapperswil
@@ -129,14 +129,18 @@ ENUM_NEXT(exchange_type_names, QUICK_MODE, IKE_SESSION_RESUME, TRANSACTION,
 	"INFORMATIONAL",
 	"IKE_SESSION_RESUME");
 #ifdef ME
-ENUM_NEXT(exchange_type_names, ME_CONNECT, ME_CONNECT, IKE_SESSION_RESUME,
+ENUM_NEXT(exchange_type_names, IKE_INTERMEDIATE, ME_CONNECT, IKE_SESSION_RESUME,
+	"IKE_INTERMEDIATE",
 	"ME_CONNECT");
-ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED,
-							   EXCHANGE_TYPE_UNDEFINED, ME_CONNECT,
+ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED, EXCHANGE_TYPE_UNDEFINED,
+		  ME_CONNECT,
 	"EXCHANGE_TYPE_UNDEFINED");
 #else
-ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED,
-							   EXCHANGE_TYPE_UNDEFINED, IKE_SESSION_RESUME,
+ENUM_NEXT(exchange_type_names, IKE_INTERMEDIATE, IKE_INTERMEDIATE,
+		  IKE_SESSION_RESUME,
+	"IKE_INTERMEDIATE");
+ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED, EXCHANGE_TYPE_UNDEFINED,
+		  IKE_INTERMEDIATE,
 	"EXCHANGE_TYPE_UNDEFINED");
 #endif /* ME */
 ENUM_END(exchange_type_names, EXCHANGE_TYPE_UNDEFINED);
@@ -217,6 +221,7 @@ METHOD(payload_t, verify, status_t,
 			}
 			break;
 		case IKE_SA_INIT:
+		case IKE_INTERMEDIATE:
 		case IKE_AUTH:
 		case CREATE_CHILD_SA:
 		case INFORMATIONAL:

bingfengfeifei/strongswan-vpp

src/libcharon/encoding/payloads/ike_header.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 Tobias Brunner
+ * Copyright (C) 2007-2019 Tobias Brunner
  * Copyright (C) 2005-2006 Martin Willi
  * Copyright (C) 2005 Jan Hutter
  * HSR Hochschule fuer Technik Rapperswil
@@ -128,15 +128,17 @@ ENUM_NEXT(exchange_type_names, QUICK_MODE, IKE_SESSION_RESUME, TRANSACTION,
 	"CREATE_CHILD_SA",
 	"INFORMATIONAL",
 	"IKE_SESSION_RESUME");
+ENUM_NEXT(exchange_type_names, IKE_INTERMEDIATE, IKE_INTERMEDIATE, IKE_SESSION_RESUME,
+	"IKE_INTERMEDIATE");
 #ifdef ME
-ENUM_NEXT(exchange_type_names, ME_CONNECT, ME_CONNECT, IKE_SESSION_RESUME,
+ENUM_NEXT(exchange_type_names, ME_CONNECT, ME_CONNECT, IKE_INTERMEDIATE,
 	"ME_CONNECT");
-ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED,
-							   EXCHANGE_TYPE_UNDEFINED, ME_CONNECT,
+ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED, EXCHANGE_TYPE_UNDEFINED,
+		  ME_CONNECT,
 	"EXCHANGE_TYPE_UNDEFINED");
 #else
-ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED,
-							   EXCHANGE_TYPE_UNDEFINED, IKE_SESSION_RESUME,
+ENUM_NEXT(exchange_type_names, EXCHANGE_TYPE_UNDEFINED, EXCHANGE_TYPE_UNDEFINED,
+		  IKE_INTERMEDIATE,
 	"EXCHANGE_TYPE_UNDEFINED");
 #endif /* ME */
 ENUM_END(exchange_type_names, EXCHANGE_TYPE_UNDEFINED);
@@ -217,6 +219,7 @@ METHOD(payload_t, verify, status_t,
 			}
 			break;
 		case IKE_SA_INIT:
+		case IKE_INTERMEDIATE:
 		case IKE_AUTH:
 		case CREATE_CHILD_SA:
 		case INFORMATIONAL: