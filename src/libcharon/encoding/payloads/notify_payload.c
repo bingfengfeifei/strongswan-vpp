wataash/strongswan

ike-qske: Change codepoints

src/libcharon/encoding/payloads/notify_payload.c
@@ -126,9 +126,7 @@ ENUM_NEXT(notify_type_names, DPD_R_U_THERE, DPD_R_U_THERE_ACK, INITIAL_CONTACT_I
 	"DPD_R_U_THERE_ACK");
 ENUM_NEXT(notify_type_names, UNITY_LOAD_BALANCE, UNITY_LOAD_BALANCE, DPD_R_U_THERE_ACK,
 	"UNITY_LOAD_BALANCE");
-ENUM_NEXT(notify_type_names, INTERMEDIATE_EXCHANGE_SUPPORTED, USE_BEET_MODE, UNITY_LOAD_BALANCE,
-	"INTERMEDIATE_EXCHANGE_SUPPORTED",
-	"ADDITIONAL_KEY_EXCHANGE",
+ENUM_NEXT(notify_type_names, USE_BEET_MODE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
 	"USE_BEET_MODE");
 ENUM_NEXT(notify_type_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_MEDIATION",
@@ -139,7 +137,13 @@ ENUM_NEXT(notify_type_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_CONNECTAUTH",
 	"ME_RESPONSE",
 	"RADIUS_ATTRIBUTE");
-ENUM_END(notify_type_names, RADIUS_ATTRIBUTE);
+ENUM_NEXT(notify_type_names, INTERMEDIATE_EXCHANGE_SUPPORTED,
+							 INTERMEDIATE_EXCHANGE_SUPPORTED, RADIUS_ATTRIBUTE,
+	"INTERMEDIATE_EXCHANGE_SUPPORTED");
+ENUM_NEXT(notify_type_names, ADDITIONAL_KEY_EXCHANGE,
+							 ADDITIONAL_KEY_EXCHANGE, INTERMEDIATE_EXCHANGE_SUPPORTED,
+	"ADDITIONAL_KEY_EXCHANGE");
+ENUM_END(notify_type_names, ADDITIONAL_KEY_EXCHANGE);
 
 
 ENUM_BEGIN(notify_type_short_names, UNSUPPORTED_CRITICAL_PAYLOAD, UNSUPPORTED_CRITICAL_PAYLOAD,
@@ -243,9 +247,7 @@ ENUM_NEXT(notify_type_short_names, DPD_R_U_THERE, DPD_R_U_THERE_ACK, INITIAL_CON
 	"DPD_ACK");
 ENUM_NEXT(notify_type_short_names, UNITY_LOAD_BALANCE, UNITY_LOAD_BALANCE, DPD_R_U_THERE_ACK,
 	"UNITY_LB");
-ENUM_NEXT(notify_type_short_names, INTERMEDIATE_EXCHANGE_SUPPORTED, USE_BEET_MODE, UNITY_LOAD_BALANCE,
-	"IKE_INT_SUP",
-	"ADD_KE",
+ENUM_NEXT(notify_type_short_names, USE_BEET_MODE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
 	"BEET_MODE");
 ENUM_NEXT(notify_type_short_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_MED",
@@ -256,7 +258,13 @@ ENUM_NEXT(notify_type_short_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE
 	"ME_CAUTH",
 	"ME_R",
 	"RADIUS");
-ENUM_END(notify_type_short_names, RADIUS_ATTRIBUTE);
+ENUM_NEXT(notify_type_short_names, INTERMEDIATE_EXCHANGE_SUPPORTED,
+								   INTERMEDIATE_EXCHANGE_SUPPORTED, RADIUS_ATTRIBUTE,
+	"IKE_INT_SUP");
+ENUM_NEXT(notify_type_short_names, ADDITIONAL_KEY_EXCHANGE,
+								   ADDITIONAL_KEY_EXCHANGE, INTERMEDIATE_EXCHANGE_SUPPORTED,
+	"ADD_KE");
+ENUM_END(notify_type_short_names, ADDITIONAL_KEY_EXCHANGE);
 
 
 typedef struct private_notify_payload_t private_notify_payload_t;

bingfengfeifei/strongswan-vpp

src/libcharon/encoding/payloads/notify_payload.c
@@ -127,8 +127,7 @@ ENUM_NEXT(notify_type_names, DPD_R_U_THERE, DPD_R_U_THERE_ACK, INITIAL_CONTACT_I
 	"DPD_R_U_THERE_ACK");
 ENUM_NEXT(notify_type_names, UNITY_LOAD_BALANCE, UNITY_LOAD_BALANCE, DPD_R_U_THERE_ACK,
 	"UNITY_LOAD_BALANCE");
-ENUM_NEXT(notify_type_names, ADDITIONAL_KEY_EXCHANGE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
-	"ADDITIONAL_KEY_EXCHANGE",
+ENUM_NEXT(notify_type_names, USE_BEET_MODE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
 	"USE_BEET_MODE");
 ENUM_NEXT(notify_type_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_MEDIATION",
@@ -139,7 +138,10 @@ ENUM_NEXT(notify_type_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_CONNECTAUTH",
 	"ME_RESPONSE",
 	"RADIUS_ATTRIBUTE");
-ENUM_END(notify_type_names, RADIUS_ATTRIBUTE);
+ENUM_NEXT(notify_type_names, ADDITIONAL_KEY_EXCHANGE,
+							 ADDITIONAL_KEY_EXCHANGE, RADIUS_ATTRIBUTE,
+	"ADDITIONAL_KEY_EXCHANGE");
+ENUM_END(notify_type_names, ADDITIONAL_KEY_EXCHANGE);
 
 
 ENUM_BEGIN(notify_type_short_names, UNSUPPORTED_CRITICAL_PAYLOAD, UNSUPPORTED_CRITICAL_PAYLOAD,
@@ -244,8 +246,7 @@ ENUM_NEXT(notify_type_short_names, DPD_R_U_THERE, DPD_R_U_THERE_ACK, INITIAL_CON
 	"DPD_ACK");
 ENUM_NEXT(notify_type_short_names, UNITY_LOAD_BALANCE, UNITY_LOAD_BALANCE, DPD_R_U_THERE_ACK,
 	"UNITY_LB");
-ENUM_NEXT(notify_type_short_names, ADDITIONAL_KEY_EXCHANGE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
-	"ADD_KE",
+ENUM_NEXT(notify_type_short_names, USE_BEET_MODE, USE_BEET_MODE, UNITY_LOAD_BALANCE,
 	"BEET_MODE");
 ENUM_NEXT(notify_type_short_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE,
 	"ME_MED",
@@ -256,7 +257,10 @@ ENUM_NEXT(notify_type_short_names, ME_MEDIATION, RADIUS_ATTRIBUTE, USE_BEET_MODE
 	"ME_CAUTH",
 	"ME_R",
 	"RADIUS");
-ENUM_END(notify_type_short_names, RADIUS_ATTRIBUTE);
+ENUM_NEXT(notify_type_short_names, ADDITIONAL_KEY_EXCHANGE,
+								   ADDITIONAL_KEY_EXCHANGE, RADIUS_ATTRIBUTE,
+	"ADD_KE");
+ENUM_END(notify_type_short_names, ADDITIONAL_KEY_EXCHANGE);
 
 
 typedef struct private_notify_payload_t private_notify_payload_t;